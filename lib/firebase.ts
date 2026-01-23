2a1be92d3bd621e93adc05443a310d6a82411431
jawad <kadhimj095@gmail.com>
2026-01-24 00:44:12 +0300

    firebase end  code

---
+++ lib/firebase.ts
@@ -0,0 +1,23 @@
+import { initializeApp, getApps, getApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+import { getAuth } from "firebase/auth"
+import { getStorage } from "firebase/storage"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB41AFLi7w4_ySww24Ai4dEUXY6hZGsOeY",
+  authDomain: "earthlink-kb-v2.firebaseapp.com",
+  projectId: "earthlink-kb-v2",
+  storageBucket: "earthlink-kb-v2.firebasestorage.app",
+  messagingSenderId: "223721088299",
+  appId: "1:223721088299:web:8cbfa879da6824cb1e916e",
+}
+
+//  Prevent re-initialization (Next.js safe)
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
+
+// Services
+export const db = getFirestore(app)
+export const auth = getAuth(app)
+export const storage = getStorage(app)
+
+export default app
