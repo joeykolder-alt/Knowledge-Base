258db94ac3b2aa9ede69b1243499291ad1cf0bda
jawad <kadhimj095@gmail.com>
2026-01-24 02:37:00 +0300

    ربط صفحة الأخبار مع Firebase

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,16 +4,18 @@ import { getAuth } from "firebase/auth"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  apiKey: "AIzaSyB41AFLi7w4_ySww24Ai4dEUXY6hZGsOeY",
+  authDomain: "earthlink-kb-v2.firebaseapp.com",
+  projectId: "earthlink-kb-v2",
+  storageBucket: "earthlink-kb-v2.firebasestorage.app",
+  messagingSenderId: "223721088299",
+  appId: "1:223721088299:web:8cbfa879da6824cb1e916e",
 }
 
+//  Prevent re-initialization (Next.js safe)
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
 
+// Services
 export const db = getFirestore(app)
 export const auth = getAuth(app)
 export const storage = getStorage(app)
